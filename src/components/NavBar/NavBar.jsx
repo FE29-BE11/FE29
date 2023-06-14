9017d0cb1c42b09c9ac4241c04c1ce7ed714c358
K4wauchi <K4wauchi@gmail.com>
2023-06-14 16:39:58 +0700

    coba tailwindcss part 1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -25,72 +25,9 @@ const NavBar = () => {
 
     return (
         <>
-            <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img src={'logo'} alt="logo" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav">
-                        <span className="navbar-toggler-icon"></span>
-                    </Navbar.Toggle>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link
-                                href="#home"
-                                className={
-                                    activeLink === 'home'
-                                        ? 'active navbar-link'
-                                        : 'navbar-link'
-                                }
-                                onClick={() => onUpdateActiveLink('home')}
-                            >
-                                Home
-                            </Nav.Link>
-                            <Nav.Link
-                                href="#Program"
-                                className={
-                                    activeLink === 'program'
-                                        ? 'active navbar-link'
-                                        : 'navbar-link'
-                                }
-                                onClick={() => onUpdateActiveLink('program')}
-                            >
-                                Program
-                            </Nav.Link>
-                            <Nav.Link
-                                href="#Donation"
-                                className={
-                                    activeLink === 'donation'
-                                        ? 'active navbar-link'
-                                        : 'navbar-link'
-                                }
-                                onClick={() => onUpdateActiveLink('donation')}
-                            >
-                                Donation
-                            </Nav.Link>
-                        </Nav>
-                        <span className="navbar-text">
-                            <div className="social-icon">
-                                <a href="">
-                                    <img src={'icon1'} alt="" />
-                                </a>
-                                <a href="">
-                                    <img src={'icon2'} alt="" />
-                                </a>
-                                <a href="">
-                                    <img src={'icon3'} alt="" />
-                                </a>
-                            </div>
-                            <button
-                                className="vvd"
-                                onClick={() => console.log('connect')}
-                            >
-                                <span>Contact Us</span>
-                            </button>
-                        </span>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <div className='text-blue'>
+                <h1>Asean Youth Ability</h1>
+            </div>
         </>
     );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createRequire } from 'module'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const require = createRequire(import.meta.url);
+const postcss = require('postcss')
+
+module.exports = {
+  plugins: [
+    // Plugin-postcss
+    require('postcss'),
+  ],
+};
